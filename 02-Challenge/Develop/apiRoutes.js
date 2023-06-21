68c8200592a2e631b2438189cff432792cc3b983
Marinah Tamura <marinahtam@gmail.com>
2023-06-21 09:52:44 -0700

    adding API routes file with added routes to retrieve the saved note and save new notes

---
+++ 02-Challenge/Develop/apiRoutes.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const path = require('path');
+const { v4: uuidv4 } = require('uuid'); // Using the `uuid` package to generate unique IDs
+
+const notesData = require('../db/db.json');
+
+module.exports = (app) => {
+  // GET route to retrieve all saved notes
+  app.get('/api/notes', (req, res) => {
+    res.json(notesData);
+  });
+
+  // POST route to save a new note
+  app.post('/api/notes', (req, res) => {
+    const newNote = req.body;
+    newNote.id = uuidv4(); // Generate a unique ID for the new note
+
+    notesData.push(newNote);
+    fs.writeFile(
+      path.join(__dirname, '../db/db.json'),
+      JSON.stringify(notesData),
+      (err) => {
+        if (err) throw err;
+        console.log('Note saved successfully!');
+        res.json(newNote);
+      }
+    );
+  });
+};
