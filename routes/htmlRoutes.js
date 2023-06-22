06f97a8ee9e5264e89449ae284c6e62823d26885
Marinah Tamura <marinahtam@gmail.com>
2023-06-21 17:52:31 -0700

    adding commentary about the express.js directory and path files in the htmlRoutes file

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,14 +1,20 @@
+//this imports the Router module from the Express framework. 
 const router = require('express').Router();
+// THis imports the Node.js 'path' module and provides functions to work with file and directory paths
 const path = require('path');
 
+//this takes the 'app' object as a parameter and is an instance of the Express application
 module.exports = (app) => {
   // Route to notes page
   app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/notes.html'));
   });
 
-  // Route to homepage
+  // Route to homepage.
+  // This defines a catch-all route with the HTTP GET method and matches any URL path that is not matched by previous routes. 
   app.get('*', (req, res) => {
+    //This sends the 'index.html' files as the response for the unmatched route. 
+    //It is a fallback so that it is still sent when no others match. 
     res.sendFile(path.join(__dirname, '../public/index.html'));
   });
 };
